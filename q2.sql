c6de77a962e0bda278ff99382043efb8bcf17d71
Matan Rafalovitz <matanraf@gmail.com>
2025-05-18 00:24:41 +0300

    removed distinct

--- q2.sql
+++ q2.sql
@@ -1,4 +1,4 @@
-select distinct number, avg(startYear-birthYear) as avgAge
+select number, avg(startYear-birthYear) as avgAge
 from memberInKnesset natural join members natural join knessets
 group by number
 order by number;
