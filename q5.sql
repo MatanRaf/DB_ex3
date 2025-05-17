2b3d1cb0a61053ada32af6708077584ed27bcac3
Matan Rafalovitz <matanraf@gmail.com>
2025-05-18 00:42:25 +0300

    added '' to female

--- q5.sql
+++ q5.sql
@@ -5,5 +5,5 @@ from memberInKnesset mik
 group by number, party
 having count(*)*0.3 <= (select count(*) 
 from (memberInKnesset natural join members) m1
-where mik.number = m1.number and mik.party = m1.party and gender = female) 
+where mik.number = m1.number and mik.party = m1.party and gender = 'female') 
 order by number, party
