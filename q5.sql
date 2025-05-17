13d3e6b023348b2e17193e509805293d697b39ba
Matan Rafalovitz <matanraf@gmail.com>
2025-05-18 00:36:16 +0300

    added select

--- q5.sql
+++ q5.sql
@@ -1,8 +1,8 @@
-select (party, number, (count(*) 
+select party, number, (select count(*) 
 from (memberInKnesset natural join members) m1
-where mik.number = m1.number and mik.party = m1.party and gender = female) / count(*) * 100 as femalePercent)
+where mik.number = m1.number and mik.party = m1.party and gender = female) / count(*) * 100 as femalePercent
 from memberInKnesset mik
-group by number,party
+group by number, party
 having count(*)*0.3 <= (select count(*) 
 from (memberInKnesset natural join members) m1
 where mik.number = m1.number and mik.party = m1.party and gender = female) 
